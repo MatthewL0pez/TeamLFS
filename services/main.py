12f4f42845462f7936652d704e8d9bf7353fba5d
Fernando Flores <fflor033104@csu.fullerton.edu>
2026-03-10 21:44:13 -0700

    Updated main.py with a pricing_service demo, improved logic for user location selection

--- services/main.py
+++ services/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import argparse
 from distance_service import DistanceService
+from pricing_service import PricingService
 
 
 # Load locations from JSON file
@@ -47,25 +48,65 @@ def main():
     city1 = args.city1
     city2 = args.city2
 
-    if not city1 or not city2:
-        print("Enter two city names (available cities listed below):")
-        for name in sorted(locations.keys()):
-            print("-", name)
+    print("\nEnter two city names (available cities listed below):")
+    for name in sorted(locations.keys()):
+        print("-", name)
+    
+    # Fernando: Split 'if not city1 or city2' into two separate conditional branch groups
+    #
+    # Also placed city1 and city2 input phases in while loops to give user another chance to
+    # input an allowed city if initial input is invalid instead of just exiting program
+
+    while True:
         if not city1:
-            city1 = input("City 1: ").strip()
+            city1 = input("City 1 (Source): ").strip()
+
+        try:
+            city1_key = find_city_key(city1, locations)
+            break
+
+        except KeyError as e:
+            print(e)
+            city1 = None
+
+
+    while True:
         if not city2:
-            city2 = input("City 2: ").strip()
+            city2 = input("City 2 (Destination): ").strip()
 
-    try:
-        city1_key = find_city_key(city1, locations)
-        city2_key = find_city_key(city2, locations)
-    except KeyError as e:
-        print(e)
-        return
+        try:
+            city2_key = find_city_key(city2, locations)
+            # New condition checks if cities are the same, and prompts user to input
+            # another valid city if source and destination are duplicates
+            if city2_key == city1_key:
+                print("Source and Destination locations cannot be the same. Try again.")
+                city2 = None
+                continue
+            break
 
+        except KeyError as e:
+            print(e)
+            city2 = None
+
+
+    # Calculates distance between city1 and city2, and prints to terminal
     distance_km = DistanceService.get_distance(city1_key, city2_key, locations)
-    print(f"Distance between {city1_key} and {city2_key}: {distance_km:.2f} km")
+    print(f"\nDistance between {city1_key} and {city2_key}: {distance_km:.2f} km")
 
+    # Calculates shipping cost based on distance and weight of package (using placeholder of 1 kg for now)
+    placeholder_weight = 1.0
+    shipping_cost = PricingService.calculate_shipping_cost(distance_km, placeholder_weight)
+    
+    # Creates an invoice dictionary that holds all relevant info about a package
+    invoice = PricingService.generate_invoice_data(city1_key, city2_key, distance_km, placeholder_weight, shipping_cost)
+
+    # Example of how package data can be displayed on terminal screen (taken from invoice dict)
+    print("\n--- SHIPPING QUOTE ---")
+    print(f"Route:        {invoice['Route']}")
+    print(f"Distance:     {invoice['Distance']}")
+    print(f"Weight:       {invoice['Weight']} (Placeholder)")   # Since package creation isn't implemented yet, all packages have placeholder weight of 1 kg
+    print(f"Total Cost:   {invoice['Total Price']}")
+    print("----------------------")
 
 if __name__ == "__main__":
     main()
